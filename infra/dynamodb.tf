3d26e1f0d36854590ae2dd7c0744868f1206d059
Danny Bozbay <107803920+dbozbay@users.noreply.github.com>
2026-05-20 18:00:35 +0100

    chore: terraform fmt

--- infra/dynamodb.tf
+++ infra/dynamodb.tf
@@ -1,8 +1,8 @@
 resource "aws_dynamodb_table" "articles" {
-  name           = var.dynamodb_table_name
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "target_name"
-  range_key      = "article_id"
+  name         = var.dynamodb_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "target_name"
+  range_key    = "article_id"
 
   attribute {
     name = "target_name"
