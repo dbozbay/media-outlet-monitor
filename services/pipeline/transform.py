ec718466b958da35c433b28bf9b5c35f5e538e3a
yusuf3435V2 <yusufahmed3435@gmail.com>
2026-05-20 12:37:44 +0100

    feat: Added function that takes in list of articles and transforms them and returns a list of dictionaries

--- services/pipeline/transform.py
+++ services/pipeline/transform.py
@@ -1,6 +1,7 @@
 import re
 from urllib.parse import urlparse
-
+from extract import scrape_articles
+from logger import configure_logging
 
 def extract_source_article_id(url: str) -> str:
     """Extracts a unique article ID from the URL based on known patterns for each source."""
@@ -43,3 +44,25 @@ def transform_article_to_dict(article) -> dict:
         "keywords": [],
         "description": article.summary,
     }
+
+
+def transform_articles(articles: list) -> list[dict]:
+    """Transforms a list of Article objects into dictionaries."""
+    return [
+        transform_article_to_dict(article)
+        for article in articles
+    ]
+
+
+if __name__ == "__main__":
+    configure_logging()
+
+    extracted_articles = scrape_articles()
+
+    transformed_articles = transform_articles(extracted_articles)
+
+    print(f"Extracted articles: {len(extracted_articles)}")
+    print(f"Transformed articles: {len(transformed_articles)}")
+
+    for article in transformed_articles[:5]:
+        print(article)
