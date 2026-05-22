c3d3a9b787033d035b664ead9705ef316ef61c62
Danny Bozbay <107803920+dbozbay@users.noreply.github.com>
2026-05-22 15:32:05 +0100

    fix: update S3 bucket names for consistency in configuration

--- infra/main.tf
+++ infra/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "c23-mesopelagic-data-staging"
+    bucket  = "c23-mesopelagic-data-terraform-state"
     key     = "terraform.tfstate"
     region  = "eu-west-2"
     encrypt = true
